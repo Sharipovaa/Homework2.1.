49be8db72128baff74ff8a4a450a0215f4ffa61d
Sharipovaa <alfya.yanbekova28@gmail.com>
2022-01-25 19:06:39 +0500

    task2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8000;
+        int  miles = 20;
+        int result = ticketPrice / miles;
+        System.out.println(result);
+
+    }
+}
